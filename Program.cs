53ba2a22764f74a5b2fae18ba5f8eb63bc11062e
pacbel <suporte@sistemavirtual.com.br>
2021-04-30 14:33:31 -0300

    202104301433

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace comprassupermercado
 {
@@ -6,7 +9,44 @@ namespace comprassupermercado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int casosTeste = Convert.ToInt16(Console.ReadLine());
+            ///Casos de teste devem estar entre 1 e 100
+            if (casosTeste < 1 || casosTeste > 100) { return; }
+
+            string[] itens = new string[casosTeste];
+            string[] itensFinais = new string[casosTeste];
+
+            for (int i = 0; i < casosTeste; i++)
+            {
+                itens[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < casosTeste; i++)
+            {
+                itens[i] = removerAcentos(itens[i]);
+                List<String> listaFinal = itens[i].Split().Distinct().ToList();
+                listaFinal.Sort();
+                string resultado = "";
+                foreach (string valor in listaFinal)
+                {
+                    resultado += valor + ' ';
+                }
+                Console.WriteLine(resultado.Substring(0, resultado.Length - 1));
+
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string removerAcentos(string texto)
+        {
+            string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for (int i = 0; i < comAcentos.Length; i++)
+            {
+                texto = texto.Replace(comAcentos[i].ToString(), semAcentos[i].ToString());
+            }
+            return texto;
         }
     }
 }
