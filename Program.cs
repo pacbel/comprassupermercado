0756b6ebc142ca2f2a88e22bf52c4144ce22793d
pacbel <suporte@sistemavirtual.com.br>
2021-04-30 10:11:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comprassupermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
